692d3d2eccf60afe9994800fcc0d5d9746f65369
eojhet <eojhet@gmail.com>
2022-09-29 14:47:37 -0400

    Finished Ch1 jump start itext7

--- src/main/java/com/eojhet/boring/App.java
+++ src/main/java/com/eojhet/boring/App.java
@@ -1,21 +1,41 @@
 package com.eojhet.boring;
 
 
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.List;
+import com.itextpdf.layout.element.ListItem;
 import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.io.font.constants.StandardFonts;
 
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class App {
 
-    public static void main( String[] args ) throws FileNotFoundException {
+    public static void main( String[] args ) throws IOException {
         PdfDocument pdf = new PdfDocument(new PdfWriter(new FileOutputStream("output/itextPDF.pdf")));
         Document document = new Document(pdf);
-        String line = "Hello! Welcome to iTextPdf CORE 7!";
-        document.add(new Paragraph(line));
+
+        PdfFont font = PdfFontFactory.createFont(StandardFonts.TIMES_ROMAN);
+        String line = "iText is:";
+        document.add(new Paragraph(line).setFont(font));
+
+        List list = new List()
+                .setSymbolIndent(12)
+                .setListSymbol("\u2022")
+                .setFont(font);
+
+        list.add(new ListItem("Nevergonna give you up"))
+                .add(new ListItem("Never gonna let you down"))
+                .add(new ListItem("Never gonna run around and desert you"))
+                .add(new ListItem("Never gonna make you cry"))
+                .add(new ListItem("Never gonna say goodbye"))
+                .add(new ListItem("Never gonna tell a lie and hurt you"));
+        document.add(list);
         document.close();
 
     }
---
+++ src/main/java/com/eojhet/boring/CsvPDF.java
@@ -0,0 +1,57 @@
+package com.eojhet.boring;
+
+import com.itextpdf.io.font.constants.StandardFonts;
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
+import com.itextpdf.kernel.geom.PageSize;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Cell;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Table;
+import com.itextpdf.layout.properties.UnitValue;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+// https://kb.itextpdf.com/home/it7kb/ebooks/itext-7-jump-start-tutorial-for-java/chapter-1-introducing-basic-building-blocks
+// https://github.com/itext/i7js-jumpstart/tree/develop/src/main/java/tutorial
+// https://api.itextpdf.com/iText7/java/7.1.16/
+
+public class CsvPDF {
+    public static final String DATA = "src/main/resources/data/united_states.csv";
+    public static void main(String[] args) throws IOException {
+        PdfWriter writer = new PdfWriter(new FileOutputStream("output/itextCSVPDF.pdf"));
+        PdfDocument pdf = new PdfDocument(writer);
+        Document document = new Document(pdf, PageSize.LETTER.rotate());
+
+        document.setMargins(20, 20, 20, 20);
+        PdfFont font = PdfFontFactory.createFont(StandardFonts.HELVETICA);
+        PdfFont bold = PdfFontFactory.createFont(StandardFonts.HELVETICA_BOLD);
+
+        Table table = new Table(new float[]{4,1,3,4,3,3,3,3,1});
+        table.setWidth(UnitValue.createPercentValue(100));
+        BufferedReader br = new BufferedReader(new FileReader(DATA));
+
+        String line = br.readLine();
+        process(table, line, bold, true);
+        while ((line = br.readLine()) != null) {
+            process(table, line, font, false);
+        }
+        br.close();
+        document.add(table);
+        document.close();
+    }
+
+    public static void process(Table table, String line, PdfFont font, boolean isHeader) {
+        StringTokenizer tokenizer = new StringTokenizer(line, ";");
+        while (tokenizer.hasMoreTokens()) {
+            if (isHeader) {
+                table.addHeaderCell(new Cell().add(new Paragraph(tokenizer.nextToken()).setFont(font)));
+            } else {
+                table.addCell(new Cell().add(new Paragraph(tokenizer.nextToken()).setFont(font)));
+            }
+        }
+    }
+}
